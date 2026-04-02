5b9229e1283f8f910b23d814c09fa24cc7a8d1bc
seungjin <xeungjin@nate.com>
2026-04-02 12:50:00 +0900

    color 변경경

--- step_3_2.py
+++ step_3_2.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from step_2_1 import OUT_DIR
 from step_3_1 import load_plot_data
 
-
 plot_data = load_plot_data()
 fig, ax = plt.subplots()
-ax.barh(plot_data["stem"], plot_data["size"])
+ax.barh(plot_data["stem"], plot_data["size"],
+ color=['yellow'])
 fig.savefig(OUT_DIR / f"{Path(__file__).stem}.png")
